f762c2cb5b2bd34bc81646f3ef5e3ccaeeee4ce2
BBMiranda <bruno.b.miranda@hotmail.com.br>
2023-01-18 14:58:00 -0300

    Fifth Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -284,3 +284,52 @@
   new PureCounter();
 
 })()
+
+/* Mascara */
+
+function mask(o,f){
+  v_obj=o
+  v_fun=f
+  setTimeout("execmask()",1)
+  }
+  
+  function execmask(){
+  v_obj.value=v_fun(v_obj.value)
+  }
+  
+  function masktel(v){
+  v=v.replace(/\D/g,"");
+  v=v.replace(/^(\d{2})(\d)/g,"($1) $2");
+  v=v.replace(/(\d)(\d{4})$/,"$1-$2");
+  return v;
+  }
+  
+  function maskcnpj(v){
+  v=v.replace(/\D/g,"");                        
+  v=v.replace(/^(\d{2})(\d)/,"$1.$2");          
+  v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3");
+  v=v.replace(/\.(\d{3})(\d)/,".$1/$2");       
+  v=v.replace(/(\d{4})(\d)/,"$1-$2");
+  return v;
+  }
+  
+  function idcss( el ){
+    return document.getElementById( el );
+  }
+  
+  window.onload = function(){
+      
+    //TEL FIXO -------
+    idcss('phone').setAttribute('maxlength', 14);
+    idcss('phone').onkeypress = function(){
+      mask( this, masktel );
+    }
+      //------------- 
+
+    //CNPJ --------
+    idcss('00NDn00000NjR66').setAttribute('maxlength', 18);
+    idcss('00NDn00000NjR66').onkeypress = function(){
+      mask( this, maskcnpj );
+    }
+    //-------------
+  }
